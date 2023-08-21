470398f0b779cfef3995d076fe199cb1b95236f2
Klown <klowninabout@gmail.com>
2023-08-21 12:44:19 -0400

    base game complete

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def pick_word():
+    words = ['hello', 'camp', 'letter']
+    # this was done bc I want to start with a list of words - change this when pulling from csv
+    return [*str(random.choice(words))]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def guess():
+    word = pick_word()
+    count = 10
+    print("Number of tries", count)
+    print("Type 'exit' to end game")
+    blanks = ['_'] * len(word)
+    while count != 0:
+        if '_' not in blanks:
+            print("Word:", "".join(blanks))
+            print("YOU WIN!!!!")
+            return
+        print(*blanks)
+        letter = input("Guess a letter: ").lower()
+        if letter == 'exit':
+            print("Thanks for playing!")
+            return
+        if letter in word:
+            while letter in word:
+                index = word.index(letter)
+                blanks[index] = letter
+                word[index] = '_'
+            if '_' in blanks:
+                print("CORRECT! (", count, "tries remaining)")
+        else:
+            count -= 1
+            print("Sorry, wrong letter... (", count, "tries remaining)")
+    print("No more tries left. You lose.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+guess()
